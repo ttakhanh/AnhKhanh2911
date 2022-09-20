7794c65689bf0d415a622fdcd10f36f87f911a8d
ttakhanh <anhkhanhtran200@gmail.com>
2022-09-20 23:10:51 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const ApiError = require("./app/api-error");
 
 const app = express();
 const contactsRouter = require("./app/routes/contact.route");
@@ -12,6 +13,15 @@ app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book application." });
 });
 
+//handle 404 response
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resoure not found"));
+});
 
+app.use((err, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Server Error",
+    });
+});
 
 module.exports = app;
